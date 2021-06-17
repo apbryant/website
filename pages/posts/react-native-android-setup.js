a9e41cf69979c9976a1be55fd98cc5133b1c50c0
Andrew Bryant <bryant.andrew01@gmail.com>
2021-06-17 19:56:02 -0400

    Update React Native post

--- pages/posts/react-native-android-setup.js
+++ pages/posts/react-native-android-setup.js
@@ -33,7 +33,8 @@ export default function ReactNativeAndroidSetup() {
 
       <article>
         <h2>React Native Android setup</h2>
-        <h3>16 June 2020</h3>
+        <h3>Created 16 June 2020</h3>
+        <h3>Updated 17 June 2020</h3>
         <p>Programming computers is hard. We’re reminded of that every day we sit down to work. But before you can even get to programming, you have to set up your computer so that the program can have a chance to run. This can be tricky, depending on matching up operating systems, packages, and versions of software.</p>
 
   <p>I had the pleasure of setting up an environment for React Native. React Native is a way to use the JavaScript library React to make iOS and Android applications. You really can use JavaScript for everything these days.</p>
@@ -46,7 +47,7 @@ export default function ReactNativeAndroidSetup() {
 
 <ul>
   <li>Your React Native project should have a directory in the project’s root called “android”. In there put a file called “local.properties” with the following line: <em>sdk.dir = /Users/YOUR_NAME/Library/Android/sdk</em> substituting <em>YOUR_NAME</em> with your username. This lets the app know where to find the software development kit (SDK) it needs.</li>
-  <li>Run <em>export ANDROID_HOME=~/Library/Android/sdk</em> in the app’s root directory. Similar to the above, tells the app where to look for the software development kit, basically a collection of software to assist in developing Android applications.</li>
+  <li>Run <em>export ANDROID_HOME=~/Library/Android/sdk</em> in the app’s root directory. Similar to the above, tells the app where to look for the software development kit, basically a collection of software to assist in developing Android applications. I find myself having to do this frequently, especially when running the application for the first time after opening the terminal.</li>
   <li>Accept all the SDK licenses. You can find them in Android Studio’s preferences:</li>
 
   <Image
@@ -63,6 +64,8 @@ export default function ReactNativeAndroidSetup() {
   <li>Specifically, you want to check Android SDK Command-line Tools (latest) and hit apply, so that you can accept the tools’ licenses.</li>
 
   <li>Generate a keystore with <em>keytool -genkey -v -keystore debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000</em> in the app subdirectory (in the android folder referenced earlier). This is where authorization and security certificates are kept.</li>
+
+  <li>Create the tools.jar file following <a href="https://stackoverflow.com/questions/56389284/execution-failed-for-task-appcompiledebugjavawithjavac-could-not-find-too/65560333#65560333">Nathan Loterio's answer to this Stack Overflow question</a></li>
 </ul>
 
   <p>With those you should solve the problems in environment setup and start developing. I hope this post helps!</p>
@@ -81,6 +84,9 @@ export default function ReactNativeAndroidSetup() {
     </li>
     <li>
       <a href="https://stackoverflow.com/questions/48017376/keystore-not-found-for-signing-config-release-react-native">Creating keystore</a>
+    </li>
+    <li>
+      <a href="https://stackoverflow.com/questions/56389284/execution-failed-for-task-appcompiledebugjavawithjavac-could-not-find-too/65560333#65560333">Creating tools.jar</a>
     </li>
   </ul>
 
