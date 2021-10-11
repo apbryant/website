3973eb19b3e670bf3f28d98748a209ff851718fa
Andrew Bryant <bryant.andrew01@gmail.com>
2021-10-11 17:42:49 -0400

    Fix typo

--- pages/cv.js
+++ pages/cv.js
@@ -11,7 +11,7 @@ export default function CV() {
       <ul>
         <li>Implemented UI changes to the platform's iOS and Angular web applications to improve the platform's accessibility and performance.</li>
         <li>Documented client APIs and integrateed them into the application.</li>
-        <li>Optimized and monitord server costs and performance.</li>
+        <li>Optimized and monitored server costs and performance.</li>
       </ul>
       <h1>ZTZ Tech Group</h1>
       <h2>Operations Manager, August 2019 - November 2020</h2>
