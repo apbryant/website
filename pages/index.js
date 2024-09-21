73c731635b8c9fbd3d3d38faca4973a82b1cdd74
Andrew Bryant <bryant.andrew01@gmail.com>
2024-09-21 17:49:32 -0400

    edit index

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,6 @@ export default function Home() {
           Welcome
         </h1>
         <p>I'm a software engineer and creative coder.</p>
-        <h1>I'm interested in</h1>
-        <ul>
-          <li>Growing as an artist and programmer</li>
-          <li>Connecting with other creative people</li>
-        </ul>
         <h1>Fun facts</h1>
         <ul>
           <li>My hobbies include Brazilian jiu jitsu, biking, and reading</li>
@@ -30,14 +25,12 @@ export default function Home() {
         <ul>
           <li>Richmond, Virginia</li>
           <li>bryant.andrew01@gmail.com</li>
-	        <li><a href="https://andrewbryant.substack.com/">Substack</a></li>
           <li><a href="https://www.instagram.com/andrew_paul_bryant/">Instagram</a></li>
           <li><a href="https://github.com/apbryant">GitHub</a></li>
         </ul>
-        <h1>Stuff I sell</h1>
+        <h1>Art stuff</h1>
         <ul>
           <li><a href="https://andrewpaulbryant.art/">Generative artwork</a></li>
-	        <li><a href="https://opensea.io/collection/teotihuacan-by-andrew-bryant">Teotihuacan by Andrew Bryant</a></li>
           <li><a href="https://gumroad.com/andrewpbryant">Ebooks about creative coding</a></li>
         </ul>
 
