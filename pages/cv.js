34b0f2ea489761a59de64561b102cf0bace49e75
Andrew Bryant <bryant.andrew01@gmail.com>
2025-11-12 09:00:07 -0500

    update cv

--- pages/cv.js
+++ pages/cv.js
@@ -107,6 +107,7 @@ export default function CV() {
 
         <h1>Group Exhibitions</h1>
         <ul>
+          <li>VA Gala Auction | Comfort Zone Camp | Richmond, Virginia, United States | December 2025</li>
           <li>Brandery Richmond | Richmond, Virginia, United States | October 2025 - April 2026</li>
           <li>Silent Auction | Little Hands Virginia | Richmond, Virginia, United States | May 2025</li>
           <li>
